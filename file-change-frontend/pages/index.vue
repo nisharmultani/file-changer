d087a6cb491112907c517218479773047af0b35a
NisharMultani <nisharmultani7@gmail.com>
2022-08-17 18:01:17 +0530

    WIP: work in png to jpg

--- file-change-frontend/pages/index.vue
+++ file-change-frontend/pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <Tutorial/>
+  <div>ok {{ this.size() }}</div>
 </template>
 
 <script>
+import imagemin from "imagemin";
+import pngToJpeg from "png-to-jpeg";
+import sizeOf from "image-size";
 export default {
-  name: 'IndexPage'
-}
+  methods: {
+    size() {
+      imagemin(["images/*.png"], "build/images", {
+        plugins: [pngToJpeg({ quality: 90 })],
+      }).then((files) => {
+        // Please keep in mind that all files now have the wrong extension
+        // You might want to change them manually
+        console.log("PNGs converted to JPEGs:", files);
+      });
+    },
+  },
+};
 </script>
